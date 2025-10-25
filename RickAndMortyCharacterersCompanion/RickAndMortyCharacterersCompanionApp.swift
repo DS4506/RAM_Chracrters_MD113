0407f58b101c10b0b1d2c735ecff53be7324c564
DS4506 <davidsmith4506@gmail.com>
2025-10-25 15:47:56 -0500

    Initial Commit

---
+++ RickAndMortyCharacterersCompanion/RickAndMortyCharacterersCompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyCharacterersCompanionApp.swift
+//  RickAndMortyCharacterersCompanion
+//
+//  Created by Willie Earl on 10/25/25.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyCharacterersCompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
